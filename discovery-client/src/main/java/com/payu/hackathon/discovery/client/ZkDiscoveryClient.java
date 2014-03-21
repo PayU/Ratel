a7b573bdf197e6d4648e36bf46fe59f42f4817e6
Michal Warecki <michal.warecki@gmail.com>
2014-03-21 13:03:05 +0100

    discovery client - fetching services implementation

--- discovery-client/src/main/java/com/payu/hackathon/discovery/client/ZkDiscoveryClient.java
+++ discovery-client/src/main/java/com/payu/hackathon/discovery/client/ZkDiscoveryClient.java
@@ -1,19 +1,64 @@
 package com.payu.hackathon.discovery.client;
 
 import com.payu.hackathon.discovery.model.Service;
+import com.payu.hackathon.discovery.model.ServiceSerializer;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 
 import java.util.Collection;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class ZkDiscoveryClient implements DiscoveryClient {
 
+    private final String zookeeperUrl;
+
+    private static final int BASE_SLEEP_TIME_MS = 1000;
+
+    private static final int MAX_RETRIES = 3;
+
+    public static final String NAMESPACE = "SERVICE_DISCOVERY";
+
+    private static final String ZK_PATH_SEPARATOR = "/";
+
+    private CuratorFramework zkClient;
+
+    ServiceSerializer serviceSerializer = new ServiceSerializer();
+
+    public ZkDiscoveryClient(String zookeeperUrl) {
+        this.zookeeperUrl = zookeeperUrl;
+        initZookeeper();
+    }
+
+    private void initZookeeper() {
+        zkClient = CuratorFrameworkFactory.builder()
+                .namespace(NAMESPACE)
+                .connectString(zookeeperUrl)
+                .retryPolicy(new ExponentialBackoffRetry(BASE_SLEEP_TIME_MS, MAX_RETRIES))
+                .build();
+        zkClient.start();
+    }
+
     @Override
     public Collection<Service> fetchAllServices() {
-        return null;
+        try {
+            return zkClient.getChildren().forPath("").stream().map(service -> fetchService(service)).collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void listenForServices(Collection<String> services, Consumer<Collection<Service>> consumer) {
 
+    }
+
+    private Service fetchService(String serviceName) {
+        try {
+            return serviceSerializer.deserializeService(zkClient.getData().forPath(serviceName));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ discovery-client/src/test/java/com/payu/hackathon/discovery/client/ZkDiscoveryClientTest.java
@@ -0,0 +1,63 @@
+package com.payu.hackathon.discovery.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.payu.hackathon.discovery.model.Service;
+import com.payu.hackathon.discovery.model.ServiceBuilder;
+import com.payu.hackathon.discovery.server.ZkDiscoveryServer;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryOneTime;
+import org.apache.curator.test.TestingServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+
+public class ZkDiscoveryClientTest {
+
+    private TestingServer zkTestServer;
+
+    private CuratorFramework client;
+
+    @Before
+    public void startZookeeper() throws Exception {
+        zkTestServer = new TestingServer(2181);
+        client = CuratorFrameworkFactory.builder()
+                .namespace(ZkDiscoveryServer.NAMESPACE)
+                .connectString(zkTestServer.getConnectString())
+                .retryPolicy(new RetryOneTime(2000))
+                .build();
+        client.start();
+    }
+
+    @After
+    public void stopZookeeper() throws Exception {
+        zkTestServer.stop();
+    }
+
+    @Test
+    public void shouldFetchServices() throws Exception {
+        //given
+        ZkDiscoveryServer zkDiscoveryServer = new ZkDiscoveryServer(zkTestServer.getConnectString());
+        ZkDiscoveryClient zkDiscoveryClient = new ZkDiscoveryClient(zkTestServer.getConnectString());
+        Service service = ServiceBuilder.aService().
+                withName("OrderService").
+                withPath("/dupsko").
+                withAddress("http://billpayments/dupsko").build();
+
+        zkDiscoveryServer.registerService(service);
+
+        //when
+        Collection<Service> services = zkDiscoveryClient.fetchAllServices();
+
+        //then
+        assertThat(services.size()).isEqualTo(1);
+        assertThat(services.stream().findFirst().get().getAddress()).isEqualTo("http://billpayments/dupsko");
+        assertThat(services.stream().findFirst().get().getName()).isEqualTo("OrderService");
+        assertThat(services.stream().findFirst().get().getPath()).isEqualTo("/dupsko");
+
+    }
+
+}
