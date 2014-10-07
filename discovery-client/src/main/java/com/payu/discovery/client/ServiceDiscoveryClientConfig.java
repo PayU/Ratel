3d2c164bcbc1d08afe5491bd4281c814218128f9
Mariusz Smykula <mariuszs@gmail.com>
2014-10-07 14:13:34 +0200

    fixed

--- discovery-client/src/main/java/com/payu/discovery/client/ServiceDiscoveryClientConfig.java
+++ discovery-client/src/main/java/com/payu/discovery/client/ServiceDiscoveryClientConfig.java
@@ -4,14 +4,24 @@ import java.io.IOException;
 import java.util.Properties;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import com.payu.discovery.RemoteService;
 
+/**
+ * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration Auto-configuration} to enable/disable Spring's
+ * {@link com.payu.discovery.client.EnableServiceDiscovery} mechanism based on configuration properties.
+ * <p>
+ * To disable auto export of annotation beans set <code>com.payu.discovery.enabled: false</code>.
+ *
+ */
+
 @Configuration
 @ConditionalOnClass({RemoteService.class})
+@ConditionalOnExpression("${com.payu.discovery.enabled:true}")
 public class ServiceDiscoveryClientConfig {
 
     public static final String DEFAULT_DISCOVERY_URL = "http://localhost:8090/server/discovery";
