6ddd62eab6006013737c33a14e5b9730470ed10b
Michal Warecki <michal.warecki@gmail.com>
2014-10-09 13:20:02 +0200

    Better metrics

--- discovery-client/src/main/java/com/payu/discovery/proxy/monitoring/MonitoringInvocationHandler.java
+++ discovery-client/src/main/java/com/payu/discovery/proxy/monitoring/MonitoringInvocationHandler.java
@@ -1,11 +1,15 @@
 package com.payu.discovery.proxy.monitoring;
 
-import com.google.common.collect.EvictingQueue;
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class MonitoringInvocationHandler implements java.lang.reflect.InvocationHandler {
 
@@ -13,32 +17,37 @@ public class MonitoringInvocationHandler implements java.lang.reflect.Invocation
 
     private Object object;
 
-    private EvictingQueue lastInvocations = EvictingQueue.create(10);
+    final MetricRegistry metrics = new MetricRegistry();
+
+    final Map<String, Timer> meters = new HashMap<>();
+
+    ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .build();
 
     public MonitoringInvocationHandler(Object object) {
         this.object = object;
+        reporter.start(30, TimeUnit.SECONDS);
     }
 
     @Override
     public Object invoke(Object o, Method method, Object[] args) throws Throwable {
-        long before = System.currentTimeMillis();
+        Timer timer = fetchTimer(method);
+
+        final Timer.Context context = timer.time();
         final Object returned = method.invoke(object, args);
-        long after = System.currentTimeMillis();
-        long took = after - before;
-        logTime(method, took);
-        logAvg(took);
+        context.stop();
+
         return returned;
     }
 
-    private void logTime(Method method, long took) {
-        LOGGER.info("Invocation of method {} took {} ms", method.getName(), took);
-    }
+    private Timer fetchTimer(Method method) {
+        if(!meters.containsKey(method)) {
+            meters.put(method.toGenericString(), metrics.timer(method.toGenericString()));
+        }
 
-    private void logAvg(long took) {
-        lastInvocations.add(took);
-        Double avarage = (Double) lastInvocations.stream().collect(Collectors.averagingLong(inv -> (Long) inv));
-        LOGGER.info("Avg time for the last {} invocations {}", lastInvocations.size(), avarage);
+        return meters.get(method.toGenericString());
     }
-
 
 }
