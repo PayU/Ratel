ad178ad5f538a84ee36521763498d01ffa449cc4
Michal Warecki <michal.warecki@gmail.com>
2014-10-09 09:34:23 +0200

    Fixed caching

--- discovery-client/src/main/java/com/payu/discovery/proxy/ProxyCache.java
+++ discovery-client/src/main/java/com/payu/discovery/proxy/ProxyCache.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 public class ProxyCache implements java.lang.reflect.InvocationHandler {
@@ -14,7 +15,7 @@ public class ProxyCache implements java.lang.reflect.InvocationHandler {
 
     private Object object;
 
-    Cache<Method, Object> cache = CacheBuilder.newBuilder()
+    Cache<MethodWithArguments, Object> cache = CacheBuilder.newBuilder()
             .maximumSize(1000)
             .expireAfterWrite(10, TimeUnit.MINUTES)
             .build();
@@ -25,18 +26,50 @@ public class ProxyCache implements java.lang.reflect.InvocationHandler {
 
     @Override
     public Object invoke(Object o, Method method, Object[] args) throws Throwable {
-        final Object cachedResults = cache.getIfPresent(method);
+        final MethodWithArguments methodWithArguments = new MethodWithArguments(method, args);
+        final Object cachedResults = cache.getIfPresent(methodWithArguments);
         if(cachedResults == null) {
             LOGGER.info("New invocation of method {}", method.getName());
             final Object results = method.invoke(object, args);
             if(results != null) {
-                cache.put(method, results);
+                cache.put(methodWithArguments, results);
             }
             return results;
         }
 
         LOGGER.info("Cached invocation of method {}", method.getName());
         return cachedResults;
+    }
+
+    private class MethodWithArguments {
+        Method method;
+        Object[] args;
+
+        private MethodWithArguments(Method method, Object[] args) {
+            this.method = method;
+            this.args = args;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            MethodWithArguments that = (MethodWithArguments) o;
+
+            // Probably incorrect - comparing Object[] arrays with Arrays.equals
+            if (!Arrays.equals(args, that.args)) return false;
+            if (!method.equals(that.method)) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = method.hashCode();
+            result = 31 * result + Arrays.hashCode(args);
+            return result;
+        }
     }
 
 }
