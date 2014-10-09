9dce2012decfe76e15ed318e5961eef734ad6730
Mariusz Smykula <mariuszs@gmail.com>
2014-10-09 12:55:50 +0200

    simplifed

--- discovery-server/src/main/java/com/payu/discovery/server/monitoring/StatusController.java
+++ discovery-server/src/main/java/com/payu/discovery/server/monitoring/StatusController.java
@@ -1,15 +1,16 @@
-package com.payu.discovery.server;
-
+package com.payu.discovery.server.monitoring;
 
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.payu.discovery.model.ServiceDescriptor;
+import com.payu.discovery.server.DiscoveryServer;
 
 @Controller
 public class StatusController {
@@ -21,13 +22,13 @@ public class StatusController {
         this.discoveryServer = discoveryServer;
     }
 
-    @RequestMapping(value = "/services", method = RequestMethod.GET)
-    public String greeting(Model model) {
-        Collection<ServiceDescriptor> services = discoveryServer.fetchAllServices();
-        if (!services.isEmpty()){
-            model.addAttribute("services", services);
-        }
+    @RequestMapping(value = "/services")
+    public String greeting() {
         return "services";
     }
 
+    @ModelAttribute("services")
+    public Collection<ServiceDescriptor> populateFeatures() {
+        return discoveryServer.fetchAllServices();
+    }
 }
