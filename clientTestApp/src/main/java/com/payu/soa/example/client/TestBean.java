ff8505634aa2b6b9ebf6ec10198b6eb6cefe4862
Michal Warecki <michal.warecki@gmail.com>
2014-10-10 15:52:13 +0200

    Discover inherits from Autowired

--- clientTestApp/src/main/java/com/payu/soa/example/client/TestBean.java
+++ clientTestApp/src/main/java/com/payu/soa/example/client/TestBean.java
@@ -5,7 +5,6 @@ import com.payu.discovery.Discover;
 import com.payu.discovery.RetryPolicy;
 import com.payu.order.server.model.Order;
 import com.payu.order.server.service.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.remoting.RemoteConnectFailureException;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +13,6 @@ import javax.annotation.PostConstruct;
 @Component
 public class TestBean {
 
-    @Autowired
     @Discover
     @Cachable
     @RetryPolicy(exception = RemoteConnectFailureException.class)
