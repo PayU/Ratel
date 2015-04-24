7d1afa44141cd0b5bb7004a5319c1a42d4e50375
lukasz.pulawski <lukasz.pulawski@payu.com>
2015-04-24 10:07:04 +0200

    Examples added

---
+++ examples/src/main/java/com/payu/ratel/examples/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package com.payu.ratel.examples.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.payu.ratel.config.EnableServiceDiscovery;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableServiceDiscovery
+@ComponentScan(basePackages={"com.payu.ratel.examples.calculator"})
+public class Calculator {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(Calculator.class, args);
+  }
+
+}
---
+++ examples/src/main/java/com/payu/ratel/examples/calculator/CalculatorService.java
@@ -0,0 +1,7 @@
+package com.payu.ratel.examples.calculator;
+
+public interface CalculatorService {
+
+  int add(int a, int b);
+
+}
