ee227f21401c8b1df3ef5d82e1b49c47bc8bfc9b
lukasz.pulawski <lukasz.pulawski@payu.com>
2015-05-05 10:24:46 +0200

    Fixed parent and maven and PMD warnings for examples module

--- examples/src/main/java/com/payu/ratel/examples/calculator/CalculatorServiceImpl.java
+++ examples/src/main/java/com/payu/ratel/examples/calculator/CalculatorServiceImpl.java
@@ -4,17 +4,16 @@ import org.springframework.stereotype.Service;
 
 import com.payu.ratel.Publish;
 
-
 @Publish
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
 
-  public int add(int a, int b) {
-    return a + b;
-  }
+    public int add(int a, int b) {
+        return a + b;
+    }
 
-  public int divide(int a, int b) {
-    return a / b;
-  }
+    public int divide(int a, int b) {
+        return a / b;
+    }
 
 }
