3ef378f28707d5df4e81602b7d760d87dfa1508d
lukasz.pulawski <lukasz.pulawski@payu.com>
2015-05-12 12:33:23 +0200

    Formatting fixed

--- tests/src/test/java/com/payu/ratel/tests/ServiceDiscoverTest.java
+++ tests/src/test/java/com/payu/ratel/tests/ServiceDiscoverTest.java
@@ -2,7 +2,8 @@
  * Copyright 2015 PayU
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
+ * use this file except in compliance with the License. You may obtain a copy
+ * of
  * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
@@ -10,7 +11,8 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * License for the specific language governing permissions and limitations
+ * under
  * the License.
  */
 package com.payu.ratel.tests;
@@ -48,7 +50,7 @@ public class ServiceDiscoverTest {
 
     @Discover
     private ProxableService proxiedService;
-    
+
     @Discover
     private SecondTestService secondService;
 
@@ -104,11 +106,11 @@ public class ServiceDiscoverTest {
         // then
         then(testServiceClient.hello()).isEqualTo("success");
     }
-    
-    @Test(expected=NoServiceInstanceFound.class)
+
+    @Test(expected = NoServiceInstanceFound.class)
     public void shouldThrowExceptionWhenNoServiceInstanceIsFound() {
-        
-        //no instasnce of this service configured
+
+        // no instasnce of this service configured
         secondService.testMethod();
     }
 
