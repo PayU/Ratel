1c64f12c0cac28787c17fe7472e468629ae8f23f
Patryk Grela <patryk.grela@payu.pl>
2015-05-14 16:45:25 +0200

    Standalone factory parameterless methods

--- core/src/main/java/com/payu/ratel/client/standalone/RatelStandaloneFactory.java
+++ core/src/main/java/com/payu/ratel/client/standalone/RatelStandaloneFactory.java
@@ -1,5 +1,8 @@
 package com.payu.ratel.client.standalone;
 
+import static com.payu.ratel.config.beans.RegistryBeanProviderFactory.SERVICE_DISCOVERY_ADDRESS;
+import static com.payu.ratel.config.beans.RegistryBeanProviderFactory.SERVICE_DISCOVERY_ZK_HOST;
+
 import com.payu.ratel.client.RatelClientProducer;
 import com.payu.ratel.config.beans.InMemoryServiceRegistryStrategy;
 import com.payu.ratel.config.beans.ZookeeperServiceRegistryStrategy;
@@ -8,8 +11,7 @@ public class RatelStandaloneFactory {
 
     private RatelClientProducer clientProducer;
 
-    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
-    public static RatelStandaloneFactory fromZookeeperServer(String zookeeperAddress) throws Exception {
+    public static RatelStandaloneFactory fromZookeeperServer(String zookeeperAddress) {
         RatelStandaloneFactory result = new RatelStandaloneFactory();
         ZookeeperServiceRegistryStrategy zookeeperStrategy = new ZookeeperServiceRegistryStrategy();
         zookeeperStrategy.configureWithZookeeperHost(zookeeperAddress);
@@ -25,6 +27,26 @@ public class RatelStandaloneFactory {
         result.clientProducer = new RatelClientProducer(ratelStrategy.getFetchStrategy(),
                 ratelStrategy.getClientProxyGenerator());
         return result;
+    }
+
+    public static RatelStandaloneFactory fromZookeeperServer() {
+        String zookeeperHost = System.getProperty(SERVICE_DISCOVERY_ZK_HOST);
+        return fromZookeeperServer(zookeeperHost);
+    }
+
+    public static RatelStandaloneFactory fromRatelServer() {
+        String ratelServer = System.getProperty(SERVICE_DISCOVERY_ADDRESS);
+        return fromRatelServer(ratelServer);
+    }
+
+    public static RatelStandaloneFactory fromAnyConfiguration() {
+        if (System.getProperty(SERVICE_DISCOVERY_ADDRESS) != null) {
+            return fromRatelServer();
+        }
+        if (System.getProperty(SERVICE_DISCOVERY_ZK_HOST) != null) {
+            return fromZookeeperServer();
+        }
+        throw new IllegalStateException("Can't resolve zookeeper address nor ratel server address.");
     }
 
     public <T> T getServiceProxy(Class<T> serviceContractClass) {
