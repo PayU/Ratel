28bd3824d2778db725b07bebf622b0a2e9bef232
Michal Krotewicz <michal.krotewicz@payu.pl>
2015-05-15 14:25:12 +0200

    [#25] removed unused import

--- core/src/main/java/com/payu/ratel/register/ServiceRegisterPostProcessor.java
+++ core/src/main/java/com/payu/ratel/register/ServiceRegisterPostProcessor.java
@@ -23,9 +23,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.ws.rs.NotFoundException;
-
-import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
